0146422ece3986e612a169d0881a4d1c70de4071
Vysakh <vysakhharikumar17@gmail.com>
2026-05-27 18:59:04 +0530

    pages read in chunks

--- document-analyzer/main.py
+++ document-analyzer/main.py
@@ -5,4 +5,14 @@ reader = PdfReader("sample.pdf")
 text = ""
 for page in reader.pages:
     text += page.extract_text()
-print(text)
+
+chunk_size = 300
+chunks = []
+for i in range(0, len(text), chunk_size):
+    chunk = text[i:i + chunk_size]
+    chunks.append(chunk)
+
+for j in range(len(chunks)):
+    print("\n---para{}---".format(j))
+    print(chunks[j])
+print("\n\n length of chunks:",len(chunks))
