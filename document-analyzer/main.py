2616c662f825a2877a02de217763f654842a56e9
Vysakh <vysakhharikumar17@gmail.com>
2026-05-30 11:56:33 +0530

    query creation as if we ask for results

--- document-analyzer/main.py
+++ document-analyzer/main.py
@@ -1,4 +1,5 @@
 from PyPDF2 import PdfReader
+from sentence_transformers import SentenceTransformer
 
 reader = PdfReader("sample.pdf")
 
@@ -7,8 +8,9 @@ for page in reader.pages:
     text += page.extract_text()
 
 chunk_size = 300
+overlap = 50
 chunks = []
-for i in range(0, len(text), chunk_size):
+for i in range(0, len(text), chunk_size - overlap):
     chunk = text[i:i + chunk_size]
     chunks.append(chunk)
 
@@ -16,3 +18,9 @@ for j in range(len(chunks)):
     print("\n---para{}---".format(j))
     print(chunks[j])
 print("\n\n length of chunks:",len(chunks))
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+embedding = model.encode(chunks[0])
+
+query = "tell me about the handwritten digit recognition project"
+query_emb =  model.encode(query)
