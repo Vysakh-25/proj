fdfabe6ec613227f6b85339c9b60e1c3f0140218
Vysakh <vysakhharikumar17@gmail.com>
2026-06-09 19:49:45 +0530

    manual replaced by automatic chunking

--- document-analyzer/main.py
+++ document-analyzer/main.py
@@ -9,18 +9,11 @@ text = ""
 for page in reader.pages:
     text += page.extract_text()
 
-chunk_size = 300
-overlap = 50
-chunks = []
-for i in range(0, len(text), chunk_size - overlap):
-    chunk = text[i:i + chunk_size]
-    chunks.append(chunk)
-
-"""for j in range(len(chunks)):
-    print("\n---para{}---".format(j))
-    print(chunks[j])
-print("\n\n length of chunks:",len(chunks))"""
-
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=300,
+    chunk_overlap=50
+)
+chunks = text_splitter.split_text(text)
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 chunk_embeddings = model.encode(chunks)
